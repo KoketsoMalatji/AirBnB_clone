3fa212039e2ceeba2f0e76f0b357749682429df9
KoketsoMalatji <koketsosean209@gmail.com>
2022-08-08 08:14:30 -0700

    Adds amenity

---
+++ models/amenity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Amenity Module contains the attributes to be assigned
+to the Amenities of the places.
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity Class
+    Attributes:
+        name (str): The Amenity name
+    """
+    name = ''
